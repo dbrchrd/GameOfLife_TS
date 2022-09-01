c1485ca189e8ef3da0f74894bb11215dabf17636
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-09-01 11:28:18 +0200

    Create main.ts
    Init HTML elements vars.

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+const resolution_range = <HTMLInputElement>(
+  document.querySelector("input.resolution")
+);
+const interval_range = <HTMLInputElement>(
+  document.querySelector("input.interval")
+);
+const start_btn = <HTMLInputElement>document.querySelector("button.start");
+const resolution_display = <HTMLDivElement>(
+  document.querySelector("div.resolution")
+);
+const interval_display = <HTMLDivElement>document.querySelector("div.interval");
