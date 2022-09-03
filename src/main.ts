bdaba5f4367b48933682a49fbc677ff1bfe58fe2
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-09-03 11:50:21 +0200

    Update main.ts
    Init resolutions and start and restart feature

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,16 @@ const resolution_display = <HTMLDivElement>(
   document.querySelector("div.resolution")
 );
 const interval_display = <HTMLDivElement>document.querySelector("div.interval");
+
+let resolutions: number[] = [2, 4, 8, 10, 20, 40, 80, 100, 200];
+let intervals: number[] = [
+  0, 10, 20, 50, 75, 100, 150, 300, 500, 750, 1000, 2000, 3000, 5000,
+];
+
+start_btn.onclick = () => {
+  if (start_btn.innerText === "Start") start_btn.innerText = "Restart";
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  resolution = resolutions[parseInt(resolution_range.value)];
+  interval = intervals[parseInt(interval_range.value)];
+  setup(resolution, interval);
+};
