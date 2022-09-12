3d59489ed433b6d1338172f3e08b9681e16a3c93
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-09-12 19:47:28 +0200

    Update game.ts
    Init HTML elements vars

---
+++ src/game.ts
@@ -0,0 +1,5 @@
+const canvas = <HTMLCanvasElement>document.querySelector("canvas#game");
+const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
+const generation_display = <HTMLSpanElement>(
+  document.querySelector("span.generation")
+);
