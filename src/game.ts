7f169797875d03c8c156a7153571d870c081bd97
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-09-12 19:50:21 +0200

    Update game.ts
    Init vars

--- src/game.ts
+++ src/game.ts
@@ -3,3 +3,14 @@ const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
 const generation_display = <HTMLSpanElement>(
   document.querySelector("span.generation")
 );
+let grid: number[][];
+let next_grid: number[][];
+let cols: number;
+let rows: number;
+let resolution: number = 2;
+let interval: number = 0; //ms
+let step: number = 0;
+let total_count: number = 0;
+let unused_square_color: string = "#111"; // hex or rgb or color name
+let dead_square_color: string = "#444"; // hex or rgb or color name
+let alive_square_color: string = "#fff"; // hex or rgb or color name
