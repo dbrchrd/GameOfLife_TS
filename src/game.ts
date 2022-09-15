b9a2a464cdbc701eed39399c249cfd67fe78e99a
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-09-15 18:51:45 +0200

    Update game.ts

--- src/game.ts
+++ src/game.ts
@@ -11,6 +11,21 @@ let resolution: number = 2;
 let interval: number = 0; //ms
 let step: number = 0;
 let total_count: number = 0;
-let unused_square_color: string = "#111"; // hex or rgb or color name
+let neverused_square_color: string = "#111"; // hex or rgb or color name
 let dead_square_color: string = "#444"; // hex or rgb or color name
 let alive_square_color: string = "#fff"; // hex or rgb or color name
+
+const fillSquare = (i: number, j: number, color: string): void => {
+  ctx.fillStyle = color;
+  ctx.fillRect(i * resolution, j * resolution, resolution - 1, resolution - 1);
+};
+
+const buildMatrix = (cols: number, rows: number): number[][] => {
+  let arr: number[][] = new Array(cols);
+
+  for (let i: number = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+
+  return arr;
+};
