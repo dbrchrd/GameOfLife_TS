aaafe0854ad8a2debb075e75f639bbc682a6197a
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-09-19 11:59:21 +0200

    Update game.ts

--- src/game.ts
+++ src/game.ts
@@ -3,6 +3,7 @@ const ctx = <CanvasRenderingContext2D>canvas.getContext("2d");
 const generation_display = <HTMLSpanElement>(
   document.querySelector("span.generation")
 );
+
 let grid: number[][];
 let next_grid: number[][];
 let cols: number;
@@ -47,4 +48,46 @@ const countAdjacentsSquares = (
 
   count -= grid[x][y];
   return count;
+};
+
+let inte: number = 0;
+const draw = (): void => {
+  total_count = grid
+    .reduce(function (a, b) {
+      return a.concat(b);
+    })
+    .reduce(function (a, b) {
+      return a + b;
+    });
+
+  generation_display.innerHTML = `Generation n°${step.toString()}`;
+
+  next_grid = buildMatrix(cols, rows);
+
+  for (let i: number = 0; i < cols; i++) {
+    for (let j: number = 0; j < rows; j++) {
+      let state: number = grid[i][j];
+
+      let neighbors: number = countAdjacentsSquares(grid, i, j);
+
+      if (state === 0 && neighbors === 3) {
+        fillSquare(i, j, alive_square_color);
+        next_grid[i][j] = 1;
+      } else if ((state === 1 && neighbors < 2) || neighbors > 3) {
+        fillSquare(i, j, dead_square_color);
+        next_grid[i][j] = 0;
+      } else {
+        next_grid[i][j] = state;
+      }
+    }
+  }
+
+  if (total_count <= 0) {
+    clearInterval(inte);
+    generation_display.innerHTML = `Life spanned ${step.toString()} generations`;
+    return;
+  }
+
+  grid = next_grid;
+  step++;
 };
